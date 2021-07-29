5c9bfc8c9c2eaef20f3560348d362ac6dfc331d0
vitorfiler <evitorfiler@gmail.com>
2021-07-29 17:26:42 -0300

    Start Project

---
+++ src/test/java/com/barbeariaapi/BarbeariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbeariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
