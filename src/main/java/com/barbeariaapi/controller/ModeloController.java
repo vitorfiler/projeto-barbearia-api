bcd3e59f7a50ea7ea64ffcf23dad0dd54fc6a240
Vitor Nunes <vnmacedo@stefanini.com>
2022-01-31 17:51:07 -0300

    ajuste token

--- src/main/java/com/barbeariaapi/controller/ModeloController.java
+++ src/main/java/com/barbeariaapi/controller/ModeloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.barbeariaapi.model.Servico;
@@ -26,7 +27,7 @@ public class ModeloController {
 	}
 	
 	@GetMapping
-	public Servico buscarPeloId(Long estabelecimentoID, Long servicoId){
+	public Servico buscarPeloId(@RequestParam(name="estabelecimento_ID")Long estabelecimentoID, Long servicoId){
 		return modeloService.buscarPeloId(estabelecimentoID, servicoId);
 	}
 	
