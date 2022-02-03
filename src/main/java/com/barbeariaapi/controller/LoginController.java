06903681beabbd4ff764650ab75cbe1d188fb80e
Vitor Nunes <vnmacedo@stefanini.com>
2022-02-03 12:08:10 -0300

    tempo do token para 20 minutos

--- src/main/java/com/barbeariaapi/controller/LoginController.java
+++ src/main/java/com/barbeariaapi/controller/LoginController.java
@@ -58,7 +58,7 @@ public class LoginController {
 								.map(GrantedAuthority::getAuthority)
 								.collect(Collectors.toList()))
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 999999999))
+				.setExpiration(new Date(System.currentTimeMillis() + 1200000))
 				.signWith(SignatureAlgorithm.HS512,
 						secretKey.getBytes()).compact();
 
