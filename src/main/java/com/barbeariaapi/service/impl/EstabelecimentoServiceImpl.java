66051d01c3cfd980d8499a13991bacb33ab86925
Vitor Macedo <evitorfiler@gmail.com>
2023-10-06 15:43:15 -0300

    ajuste contratação de plano

--- src/main/java/com/barbeariaapi/service/impl/EstabelecimentoServiceImpl.java
+++ src/main/java/com/barbeariaapi/service/impl/EstabelecimentoServiceImpl.java
@@ -25,114 +25,107 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component("EstabelcimentoController")
-public class EstabelecimentoServiceImpl implements EstabelecimentoService{
+public class EstabelecimentoServiceImpl implements EstabelecimentoService {
 
 	@Autowired
 	EstabelecimentoRepository estabelecimentoRepository;
-	
+
 	@Autowired
 	EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	PlanoRepository planoRepository;
-	
+
 	public void cadastrarEstabelecimento(@RequestBody Estabelecimento estabelecimento) throws Exception {
 		Estabelecimento estabelecimentoExistente = estabelecimentoRepository.findByEmail(estabelecimento.getEmail());
-		if(estabelecimentoExistente == null) {			
+		if (estabelecimentoExistente == null) {
 			estabelecimentoRepository.save(estabelecimento);
-		}else {			
-			throw new Exception("Usuário com email "+estabelecimento.getEmail()+" já existe!");
+		} else {
+			throw new Exception("Usuário com email " + estabelecimento.getEmail() + " já existe!");
 		}
 	}
-	
+
 	public Optional<Estabelecimento> buscarEstabelecimentoPeloId(Long id) {
-		try {			
+		try {
 			Optional<Estabelecimento> estabelecimento = estabelecimentoRepository.findById(id);
-			if(estabelecimento.isPresent()) {				
+			if (estabelecimento.isPresent() && estabelecimento.get().getEnderecoID() != null) {
 				Optional<Endereco> endereco = enderecoRepository.findById(estabelecimento.get().getEnderecoID());
 				estabelecimento.get().setEndereco(endereco.get());
 //				Optional<Plano> plano = planoRepository.findById(estabelecimento.get().getPlanoID());
 //				estabelecimento.get().setPlano(plano.get());
-				
 			}
 			return estabelecimento;
 		} catch (Exception e) {
 			throw new IllegalArgumentException("Falha ao buscar estabelecimento", e);
-		}		
+		}
 	}
 
-	
 	public Map<String, String> recuperarSenha(String email) {
 		Map<String, String> parametros = new HashMap<>();
 		Estabelecimento estabelecimento = estabelecimentoRepository.findByEmail(email);
-		if(estabelecimento != null) {
+		if (estabelecimento != null) {
 			EmailUtils.enviarEmail(email);
 			parametros.put("status", "Email enviado com sucesso!");
 			parametros.put("hash", gerarHashRecuperacao(email));
-		}else {
+		} else {
 			parametros.put("resposta", "o Email: " + email + " não encontrado no sistema!");
 		}
-		
+
 		return parametros;
-	}	
-	
+	}
+
 	public String redefinirSenha(Map<String, String> parametros) {
-		String senha  = parametros.get("password");
-		String email  = parametros.get("email");
+		String senha = parametros.get("password");
+		String email = parametros.get("email");
 		Estabelecimento estabelecimento = estabelecimentoRepository.findByEmail(email);
-		if(estabelecimento != null) {
+		if (estabelecimento != null) {
 			estabelecimento.setSenha(senha);
 			estabelecimentoRepository.save(estabelecimento);
 			return "Senha alterada com sucesso!";
-		}else {
+		} else {
 			return "Falha ao alterar senha!";
 		}
 	}
-	
+
 	public Estabelecimento atualizarEstabelecimento(Estabelecimento estabelecimento) throws Exception {
 		Estabelecimento estabelecimentoResponse = estabelecimentoRepository.findByEmail(estabelecimento.getEmail());
-		if(estabelecimentoResponse != null) {
+		if (estabelecimentoResponse != null) {
 			estabelecimentoRepository.save(estabelecimento);
-			Optional<Endereco> endereco = enderecoRepository.findById(estabelecimento.getEnderecoID());
-			if(endereco != null) {
-				estabelecimento.setEndereco(endereco.get());
+			if(estabelecimento.getEnderecoID() != null) {
+				Optional<Endereco> endereco = enderecoRepository.findById(estabelecimento.getEnderecoID());
+				if (endereco != null) {
+					estabelecimento.setEndereco(endereco.get());
+				}
 			}
 			return estabelecimento;
-		}else {
-			throw new Exception("Usuário com email "+estabelecimento.getEmail()+" não foi encontrado!");
+		} else {
+			throw new Exception("Usuário com email " + estabelecimento.getEmail() + " não foi encontrado!");
 		}
 	}
-	
+
 	private String gerarHashRecuperacao(String username) {
 		String secretKey = "mySecretKey";
-		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
-				.commaSeparatedStringToAuthorityList("ROLE_USER");
-		
-		String hash = Jwts
-				.builder()
-				.setId("softtekJWT")
-				.setSubject(username)
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+
+		String hash = Jwts.builder().setId("softtekJWT").setSubject(username)
 				.claim("authorities",
-						grantedAuthorities.stream()
-								.map(GrantedAuthority::getAuthority)
-								.collect(Collectors.toList()))
+						grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
 				.setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + 600000))
-				.signWith(SignatureAlgorithm.HS512,
-						secretKey.getBytes()).compact();
+				.signWith(SignatureAlgorithm.HS512, secretKey.getBytes()).compact();
 
 		return hash;
 	}
-	
+
 	public Estabelecimento completarCadastroEstabelecimento(Map<String, String> params, Long estabelecimentoID) {
 		try {
 			Optional<Estabelecimento> estabelecimento = estabelecimentoRepository.findById(estabelecimentoID);
-			Long enderecoID= Long.parseLong(params.get("enderecoID")); 
-			Boolean cadastroCompleto = Boolean.valueOf(params.get("cadastroCompleto")); 
-			if(estabelecimento.isPresent()) {
+			Long enderecoID = Long.parseLong(params.get("enderecoID"));
+			Boolean cadastroCompleto = Boolean.valueOf(params.get("cadastroCompleto"));
+			if (estabelecimento.isPresent()) {
 				estabelecimento.get().setEnderecoID(enderecoID);
 				estabelecimento.get().setCadastroCompleto(cadastroCompleto);
-				
+
 				estabelecimentoRepository.save(estabelecimento.get());
 				Optional<Endereco> endereco = enderecoRepository.findById(enderecoID);
 				estabelecimento.get().setEndereco(endereco.get());
