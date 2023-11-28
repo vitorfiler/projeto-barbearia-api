43b2c1b24b959d07f316fc8ef07a48ab006820f5
Vitor Macedo <evitorfiler@gmail.com>
2023-11-28 14:01:56 -0300

    finalizando crud provisório

--- src/main/java/com/barbeariaapi/dominadash/CompilacaoConstroller.java
+++ src/main/java/com/barbeariaapi/dominadash/CompilacaoConstroller.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +23,24 @@ public class CompilacaoConstroller {
 	CompilacaoRepository compilacaoRepository;
 	
 	@PostMapping()
-	public ResponseEntity<Compilacao> adicionarCompilacao(@RequestBody Compilacao compilacao) throws Exception {
+	public ResponseEntity<Compilacao> adicionar(@RequestBody Compilacao compilacao) throws Exception {
 		try {
 			compilacaoRepository.save(compilacao);
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+		return new ResponseEntity<>(compilacao, HttpStatus.OK);
+	}
+	
+	@PutMapping()
+	public ResponseEntity<Compilacao> atualizar(@RequestBody Compilacao compilacao) throws Exception {
+		try {
+			if (compilacao != null && compilacao.getId() != null) {
+				Compilacao response = compilacaoRepository.findById(compilacao.getId()).get();
+				if(response != null) {					
+					compilacaoRepository.save(compilacao);
+				}
+			}
 		} catch (Exception e) {
 			throw new Exception(e);
 		}
@@ -38,6 +56,15 @@ public class CompilacaoConstroller {
 			throw new Exception(e);
 		}
 		return new ResponseEntity<>(compilacoes, HttpStatus.OK);
+	}
+	
+	@DeleteMapping
+	public void deletar(@RequestParam(name="id") Long id) throws Exception {
+		try {
+			compilacaoRepository.deleteById(id);
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
 	}
 		
 }
